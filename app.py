a7524602054877f95fc94079f22e8b431df513f7
Cristian <ccaro5551@gmail.com>
2024-08-04 01:13:56 -0300

    Ultimos Cambios

--- app.py
+++ app.py
@@ -15,10 +15,10 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-app.config['MAIL_SERVER'] = 'smtp.example.com'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
-app.config['MAIL_USERNAME'] = 'your-email@example.com'
-app.config['MAIL_PASSWORD'] = 'your-password'
+app.config['MAIL_USERNAME'] = 'aestheticestetica22@gmail.com'
+app.config['MAIL_PASSWORD'] = 'esteticaaesthetic2024'
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 mail = Mail(app)
