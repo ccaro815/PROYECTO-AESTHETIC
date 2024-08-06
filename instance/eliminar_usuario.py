ff302bb6e52756af17788c8c6a9ce461f0a9f3bd
Cristian <ccaro5551@gmail.com>
2024-08-06 16:48:04 -0300

    Actualizacion

--- instance/eliminar_usuario.py
+++ instance/eliminar_usuario.py
@@ -16,24 +16,4 @@ eliminar_usuario(user_id)
 
 conn.close()
 
-#Mostrar tablas con los registros borrados o los que queres borrar
-import sqlite3
-
-db_path = 'C:\\Users\\Flia Caro Sosa Veron\\Desktop\\PROYECTO-AESTHETIC\\instance\\users.db'
-conn = sqlite3.connect(db_path)
-cursor = conn.cursor()
-
-def mostrar_usuarios():
-    select_query = "SELECT * FROM user;"
-    cursor.execute(select_query)
-    usuarios = cursor.fetchall()
-    if usuarios:
-        for usuario in usuarios:
-            print(usuario)
-    else:
-        print("No hay usuarios en la base de datos.")
-
-mostrar_usuarios()
-
-conn.close()
 
