1e547b1560d22e4e4ba4bc65f5e6d2aa2ae1a291
Cristian <ccaro5551@gmail.com>
2024-10-14 14:11:27 -0300

    Scrum Master Arreglando Cagadas

--- static/scripts/Servicios.js
+++ static/scripts/Servicios.js
@@ -1,13 +1,15 @@
-const stars = document.querySelectorAll(".star");
+document.querySelectorAll('.card-product').forEach(function(productCard) {
+  const stars = productCard.querySelectorAll('.star');
+  
+  stars.forEach(function(star, index) {
+    star.addEventListener('click', function() {
+      for (let i = 0; i <= index; i++) {
+        stars[i].classList.add('checked');
+      }
+      for (let i = index + 1; i < stars.length; i++) {
+        stars[i].classList.remove('checked');
+      }
+    });
+  });
+});
 
-stars.forEeach(function(star, index) {
-  star.addEventListener('click', function() {
-    for (let i = 0; i<=index; i++) {
-      stars[i] .classList.add('checked');
-    }
-    for (let i = index+1; i<stars.length; i++){
-      stars[i] .classList.remove('checked');
-    }
-
-  })
-})
